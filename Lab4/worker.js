4c4295abcc9a2159830a0fddeceacd1858a83bac
16sakuraa <64010860@kmitl.ac.th>
2023-02-14 22:36:26 +0700

    auto refresh Date and Time

--- Lab4/worker.js
+++ Lab4/worker.js
@@ -2,10 +2,10 @@ addEventListener("message", function(event) {
   if (event.data === "start") {
     setInterval(function() {
       var now = new Date();
-      var timeString = now.toLocaleTimeString();
-      var dateString = now.toLocaleDateString();
-      var dateTimeString = timeString + " on " + dateString;
-      postMessage(dateTimeString);
+    //   var timeString = now.toLocaleTimeString();
+    //   var dateString = now.toLocaleDateString();
+    //   var dateTimeString = timeString + " on " + dateString;
+      postMessage(now);
     }, 1000);
   }
 });
