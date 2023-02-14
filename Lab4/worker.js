8594603e462e4542beaab348aedb1ae155a42d9d
16sakuraa <64010860@kmitl.ac.th>
2023-02-14 23:05:34 +0700

    text color cycle

--- Lab4/worker.js
+++ Lab4/worker.js
@@ -2,9 +2,6 @@ addEventListener("message", function(event) {
   if (event.data === "start") {
     setInterval(function() {
       var now = new Date();
-    //   var timeString = now.toLocaleTimeString();
-    //   var dateString = now.toLocaleDateString();
-    //   var dateTimeString = timeString + " on " + dateString;
       postMessage(now);
     }, 1000);
   }
