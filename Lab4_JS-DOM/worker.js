878c7c54157a48651704218b9e87a6b7abb5e986
16sakuraa <64010860@kmitl.ac.th>
2023-02-16 09:33:06 +0700

    fix delay

--- Lab4_JS-DOM/worker.js
+++ Lab4_JS-DOM/worker.js
@@ -1,5 +1,7 @@
 addEventListener("message", function(event) {
   if (event.data === "start") {
+    var now = new Date();
+    postMessage(now);
     intervalID = setInterval(function() {
       var now = new Date();
       postMessage(now);
